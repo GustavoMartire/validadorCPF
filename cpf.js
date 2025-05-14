3d3c3ca473b6750198f229835962139f1d7a9b01
Gustavo <gustavobmartire@gmail.com>
2025-05-13 22:04:09 -0300

    Isso ai mesmo

---
+++ cpf.js
@@ -0,0 +1,60 @@
+function validarCPF(cpf){
+    cpf = cpf.replace(/[^\d]+/g,"");
+
+    if(cpf.length !== 11 || /[^\d]+\1+$/.test(cpf)){
+        return false;
+    }
+
+    let soma = 0;
+    let resto;
+
+    // Validação do 1 DV
+    for(let i=1; i <= 9; i++){
+        soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
+    }
+
+    resto = (soma * 10) % 11
+
+    if(resto === 10 || resto === 11)
+    {
+        resto = 0
+    }
+    if(resto !== parseInt(cpf.substring(9, 10))){
+        return false;
+    }
+
+    // Validação do 2 DV
+    soma = 0;
+    for(let i = 1; i <=10; i++){
+        soma += parseInt(cpf.substring(i -1, i)) * (12 - i);
+
+    }
+    resto = (soma * 10) % 11;
+
+    if(resto === 10 || resto === 11)
+    {
+        resto = 0;
+    }
+    if(resto !== parseInt(cpf.substring(10,11))){
+        return false;
+    }
+
+    return true; 
+ }
+
+ document.getElementById("cpfForm");
+ addEventListener("submit", function(e){
+    e.preventDefault();
+    const cpfInput = document.getElementById("cpf").value;
+    const messageDiv = document.getElementById("message");
+
+    if(validarCPF(cpfInput)){
+        messageDiv.textContent = "CPF Válida";
+        messageDiv.className = "message sucess";
+    } else{
+        messageDiv.textContent = "CPF Inválido";
+        messageDiv.className ="message error";
+    }
+
+    messageDiv.style.display = "block";
+ });
